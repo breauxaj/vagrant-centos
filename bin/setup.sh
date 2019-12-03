1691b8604c9af83db76c6c07db20210cc197e7b7
Adam Breaux <adam.breaux@gopuff.com>
2019-12-03 12:47:05 -0500

    upgrade to v8

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,12 +2,8 @@
 ################################################################################
 # Add EPEL repository
 ################################################################################
-curl -sOL https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -y -q install epel-release-latest-7.noarch.rpm > /dev/null 2>&1
-################################################################################
-# Install Packages
-################################################################################
-yum -y -q install deltarpm git > /dev/null 2>&1
+curl -sOL https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+yum -y -q install epel-release-latest-8.noarch.rpm > /dev/null 2>&1
 ################################################################################
 # Cleanup
 ################################################################################
